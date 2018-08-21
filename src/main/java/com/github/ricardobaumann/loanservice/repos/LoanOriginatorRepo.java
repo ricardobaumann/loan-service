2691181a8bcd4ed1b7415918e0f2a78586f49da6
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-08-21 10:02:29 +0200

    role base authorization support

--- src/main/java/com/github/ricardobaumann/loanservice/repos/LoanOriginatorRepo.java
+++ src/main/java/com/github/ricardobaumann/loanservice/repos/LoanOriginatorRepo.java
@@ -4,8 +4,10 @@ import com.github.ricardobaumann.loanservice.models.LoanOriginator;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface LoanOriginatorRepo extends CrudRepository<LoanOriginator, Long> {
     LoanOriginator findByName(String name);
 }
--- src/main/java/com/github/ricardobaumann/loanservice/repos/LoanRepo.java
+++ src/main/java/com/github/ricardobaumann/loanservice/repos/LoanRepo.java
@@ -4,7 +4,9 @@ import com.github.ricardobaumann.loanservice.models.Loan;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface LoanRepo extends CrudRepository<Loan, Long> {
 }
