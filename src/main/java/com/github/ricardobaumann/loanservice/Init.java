a20f182b417ac20f929341ee7abe7f1503c5d338
Ricardo Luis Baumann <gemini.richard@gmail.com>
2018-08-21 10:17:17 +0200

    role_based_authorization (#1)
    * role base authorization support
    
    * role base authorization support

--- src/main/java/com/github/ricardobaumann/loanservice/Init.java
+++ src/main/java/com/github/ricardobaumann/loanservice/Init.java
@@ -1,14 +1,10 @@
 package com.github.ricardobaumann.loanservice;
 
-import com.github.ricardobaumann.loanservice.models.Loan;
-import com.github.ricardobaumann.loanservice.models.LoanOriginator;
 import com.github.ricardobaumann.loanservice.repos.LoanOriginatorRepo;
 import com.github.ricardobaumann.loanservice.repos.LoanRepo;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
 
 @Component
 public class Init implements CommandLineRunner {
@@ -23,12 +19,6 @@ public class Init implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        LoanOriginator firstLoanOriginator = new LoanOriginator(1, "first");
-        LoanOriginator secondLoanOriginator = new LoanOriginator(2, "second");
-        loanOriginatorRepo.save(firstLoanOriginator);
-        loanOriginatorRepo.save(secondLoanOriginator);
-
-        loanRepo.save(new Loan(1L, new BigDecimal(100.0), firstLoanOriginator, new BigDecimal(3.0)));
-        loanRepo.save(new Loan(2L, new BigDecimal(200.0), secondLoanOriginator, new BigDecimal(5.0)));
+       
     }
 }
--- src/main/java/com/github/ricardobaumann/loanservice/repos/InvestmentsRepo.java
+++ src/main/java/com/github/ricardobaumann/loanservice/repos/InvestmentsRepo.java
@@ -4,7 +4,9 @@ import com.github.ricardobaumann.loanservice.models.Investment;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource
+@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
 public interface InvestmentsRepo extends CrudRepository<Investment, Long> {
 }
